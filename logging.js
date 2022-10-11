593b41d1b4491349c574aa7a4dfbca29e062a209
bblake <bblake@goempyrean.com>
2022-10-11 09:51:56 -0500

    ignore

--- logging.js
+++ logging.js
@@ -4,7 +4,7 @@ import winston from 'winston'
 const date = new Date().toISOString();
 const logFormat = winston.format.printf((info) => {
   // console.warn(info)
-  return `${date}-${info.level}: ${JSON.stringify(info.message, null, 4)}; query: ${JSON.stringify(info.meta.req.query, null, 4)}; status: ${JSON.stringify(info.meta.res.statusCode, null, 4)}; responseTime: ${JSON.stringify(info.meta.responseTime, null, 4)}\n`
+  return `${date}-${info.level}: ${JSON.stringify(info.message, null, 4)};\n query: ${JSON.stringify(info.meta.req.query, null, 4)};\n status: ${JSON.stringify(info.meta.res.statusCode, null, 4)};\n responseTime: ${JSON.stringify(info.meta.responseTime, null, 4)}\n`
 }
 );
 
